3c19200e7ad9df84067e928725f140a6df58b9bf
Bahareh Gholami <bahareh.gh001@gmail.com>
2025-09-27 15:08:47 +0330

    edit seed syntax

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -7,10 +7,10 @@ const sql = postgres(process.env.POSTGRES_URL!, {ssl:'require'});
 export async function seedUsers(){
     await sql `CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
     await sql `CREATE TABLE IF NOT EXISTS users (
-    IF UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email TEXT NOT NULL UNIQUE,
-    password TESX NOU NULL) `;
+    password TExt NOU NULL) `;
 
     console.log('user table created');
 
@@ -18,9 +18,9 @@ export async function seedUsers(){
         users.map (async (user) => {
         const hashPassword = await bcrypt.hash(user.password, 10)
         return sql`
-        INSERT INTO users(id. name, email, password)
-        VALUE (${user.id}, ${user.name}, ${user.email}, ${hashPassword})
-        ON CONFLICT id DO NOTHING`
+        INSERT INTO users(id, name, email, password)
+        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashPassword})
+        ON CONFLICT (id) DO NOTHING`
     })
     );
     console.log('data added');
@@ -42,10 +42,10 @@ export async function seedTasks(){
     const insertTasks = await Promise.all (
         tasks.map((task) => sql `
             INSERT INTO tasks (id, user_id, title, description, category)
-            VALUE(${task.id}, ${task.user_id}, ${task.title}, ${task.description}, ${task.category})
+            VALUES(${task.id}, ${task.user_id}, ${task.title}, ${task.description}, ${task.category})
             ON CONFLICT (id) DO NOTHING;`
         )
-    )
+    );
 }
 
 export async function GET(){
